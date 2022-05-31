9ebab289fe3b16d99e9c7a99b2eb2be3921c3cea
nlevinn <106302980+nlevinn@users.noreply.github.com>
2022-05-31 21:26:25 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,14 @@
+
+    var mySwiper = new Swiper(".slider_work", {
+      spaceBetween: 72,
+      slidesPerView: 2.2,
+      centeredSlides: true,
+      roundLengths: true,
+      loop: true,
+      loopAdditionalSlides: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+      }
+    });
+  
--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-var swiper = new Swiper('.swiper', {
-    spaceBetween: 24,
-    navigation: { // задаем кнопки навигации
-		nextEl: '.slider-button-next', // кнопка Next
-		prevEl: '.slider-button-prev' // кнопка Prev
-	},
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        type : 'custom',
-        bulletClass:'button_slide__items'
-      },
-    
-  });
+
+
+	swiperTabsNav = new Swiper('.swiper-tabs-nav', {
+        spaceBetween: 0,
+        slidesPerView: 'auto',
+        loop: false,
+        loopedSlides: 5,
+        autoHeight: false,
+        resistanceRatio: 0,
+        watchOverflow: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+        setWrapperSize: true,
+    });
+
+    // Swiper Content
+    swiperTabsContent = new Swiper('.swiper-tabs-content', {
+        spaceBetween: 30,
+        loop:true,
+        autoHeight: true,
+        longSwipes: true,
+        resistanceRatio: 0, 
+        watchOverflow: true,
+        loopedSlides: 5,
+        thumbs: {
+            swiper: swiperTabsNav,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+    });
+
+
