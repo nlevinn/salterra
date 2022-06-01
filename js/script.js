cea1d62ff835b80a91d9bb33c5dc5587eb277c1e
nlevinn <106302980+nlevinn@users.noreply.github.com>
2022-06-01 13:01:10 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -32,3 +32,66 @@
     });
 
 
+
+    document.addEventListener('DOMContentLoaded', function() {
+
+        const selector = '.we_button';
+        const elems = Array.from(document.querySelectorAll(selector));
+        const navigation = document.querySelector('.we_button_items');
+  
+        function makeActive(evt) {
+          const target = evt.target;
+  
+           if (!target || !target.matches(selector)) {
+             return;
+           }
+  
+          elems.forEach(elem => elem.classList.remove('we_button_active'));
+          evt.target.classList.add('we_button_active');
+        };
+  
+        navigation.addEventListener('mousedown', makeActive);
+  
+      });
+
+
+      window.addEventListener("DOMContentLoaded", function() {
+        [].forEach.call( document.querySelectorAll('.tell'), function(input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function(a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function(a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5)  this.value = ""
+        }
+    
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    
+      });
+    
+    });
+
+    $(document).ready(function(){   
+        $("#email").inputmask("email")
+      });
