f48450da73ae5288dec967eff7e52aaf67b54651
nlevinn <106302980+nlevinn@users.noreply.github.com>
2022-06-09 18:40:05 +0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -7,8 +7,8 @@
       loop: true,
       loopAdditionalSlides: 30,
       navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
+        nextEl: ".swiper-phone-next",
+        prevEl: ".swiper-phone-prev"
       },
       breakpoints :{
         998: {
@@ -58,63 +58,77 @@
       },
       });
 
-
-      var mySwiper = new Swiper(".team_item", 
-      {
-        spaceBetween: 80,
-        slidesPerView: 2.2,
-        centeredSlides: true,
-        roundLengths: true,
-        watchOverflow: true,
-        loop: true,
-        loopAdditionalSlides: 30,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev"
-        },
-        breakpoints :{
-          998: {
+      document.addEventListener('DOMContentLoaded', () => {
+        const width = window.innerWidth
+        if (width < 1000){
+          var mySwiper = new Swiper(".team_item", 
+          {
+            spaceBetween: 80,
             slidesPerView: 2.2,
-          },
-          768:{
-            slidesPerView: "auto",
-          },
-          425:{
-            slidesPerView: "auto",
-          },
-          320:{
-            slidesPerView: "auto",
-          }
-        },
-        });
+            centeredSlides: true,
+            roundLengths: true,
+            watchOverflow: true,
+            loop: true,
+            loopAdditionalSlides: 30,
+            navigation: {
+              nextEl: ".swiper-team-next",
+              prevEl: ".swiper-team-prev"
+            },
+            breakpoints :{
+              998: {
+                slidesPerView: "auto",
+                spaceBetween: 10,
+              },
+              768:{
+                slidesPerView: "auto",
+                spaceBetween: 30,
+              },
+              425:{
+                slidesPerView: "auto",
+              },
+              320:{
+                slidesPerView: "auto",
+              }
+            },
+            });
+        }
+      })
 
-        var mySwiper = new Swiper(".reviews_video", 
-        {
-          spaceBetween: 60,
-          slidesPerView: 2.2,
-          centeredSlides: true,
-          roundLengths: true,
-          loop: true,
-          loopAdditionalSlides: 30,
-          navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev"
-          },
-          breakpoints :{
-            998: {
-              slidesPerView: 2.2,
+
+
+      document.addEventListener('DOMContentLoaded', () => {
+        const width = window.innerWidth
+        if (width < 998){
+          var mySwiper = new Swiper(".reviews_video", 
+          {
+            spaceBetween: 60,
+            slidesPerView: 2.2,
+            centeredSlides: true,
+            roundLengths: true,
+            loop: true,
+            loopAdditionalSlides: 30,
+            navigation: {
+              nextEl: ".swiper-team-next",
+              prevEl: ".swiper-team-prev"
             },
-            768:{
-              slidesPerView: "auto",
+            breakpoints :{
+              998: {
+                slidesPerView: 2.2,
+              },
+              768:{
+                slidesPerView: "auto",
+              },
+              425:{
+                slidesPerView: "auto",
+              },
+              320:{
+                slidesPerView: "auto",
+              }
             },
-            425:{
-              slidesPerView: "auto",
-            },
-            320:{
-              slidesPerView: "auto",
-            }
-          },
-          });
+            });
+        }
+      })
+       
 
 
   
