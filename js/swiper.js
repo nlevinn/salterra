9ad0e0f3c1de1d39e3e2aa2e537e4f15933b3e00
nlevinn <106302980+nlevinn@users.noreply.github.com>
2022-06-11 21:43:16 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -9,6 +9,20 @@
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
+            breakpoints :{
+        998: {
+          spaceBetween : 20,
+        },
+        768:{
+          spaceBetween : 20,
+        },
+        425:{
+          spaceBetween : 20,
+        },
+        320:{
+          spaceBetween : 20,
+        }
+      },
       });
 
   
